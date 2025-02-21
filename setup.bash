83fa545ebd2e2153c5643965a4fcef5ee65a5528
s-takahashijp <shallow.e1621@gmail.com>
2025-02-21 09:00:16 +0900

    2/21 gnss 統合とエラー処理

--- setup.bash
+++ setup.bash
@@ -1,11 +1,14 @@
-# export CMAKE_PREFIX_PATH=/opt/vtk8
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/vtk8/lib
-# export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/pcl
-
 #!/bin/bash
-posefile="data/ATC_area_2/init_pose.txt"
+export CMAKE_PREFIX_PATH=/opt/vtk8
+sleep 1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/vtk8/lib
+sleep 1
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/pcl
 
-while IFS=, read -r init_pose1 init_pose2 init_pose3 init_pose4 init_pose5 init_pose6 init_pose7
-do
- echo "${init_pose1} ${init_pose2} ${init_pose3} ${init_pose3} ${init_pose4} ${init_pose5} ${init_pose6} ${init_pose7}"
-done < ${posefile}
+
+# posefile="data/ATC_area_2/init_pose.txt"
+
+# while IFS=, read -r init_pose1 init_pose2 init_pose3 init_pose4 init_pose5 init_pose6 init_pose7
+# do
+#  echo "${init_pose1} ${init_pose2} ${init_pose3} ${init_pose3} ${init_pose4} ${init_pose5} ${init_pose6} ${init_pose7}"
+# done < ${posefile}
